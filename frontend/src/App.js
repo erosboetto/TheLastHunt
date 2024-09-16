b3806a33ce10b085d34dd38e5e6220ca797e799e
erosboetto <erosboetto23@gmail.com>
2024-09-16 23:09:35 +0200

    #

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import MyNavBar from './components/Navbar/MyNavBar';
+import Sidebar from './components/Sidebar/SideBar';
+import { fetchCurrentUser } from './api/api';
+import { useState, useEffect, useContext } from 'react';
 import './App.css';
+import EditExperiencePage from './components/Experience/EditExperiencePage';
+import MyFooter from './components/Footer/MyFooter';
+import Profile from './components/NewProfile/Profile';
+import NotFound from './components/NotFound';
+import Login from './components/Login/Login';
+import { LoginContext } from './contexts/LoginContextProvider';
+import ProtectedRoutes from './routes/ProtectedRoutes';
+import GuestRoutes from './routes/GuestRoutes';
 
 function App() {
+  const [currentUserId, setCurrentUserId] = useState(null);
+  const {token} = useContext(LoginContext)
+
+  useEffect(() => {
+    if(!token) return
+    const loadCurrentUser = async () => {
+      try {
+        const userData = await fetchCurrentUser(token);
+        setCurrentUserId(userData._id);
+      } catch (error) {
+        console.error('Impossibile recuperare i dati dell\'utente:', error);
+      }
+    };
+
+    loadCurrentUser();
+  }, [token]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    {token && <MyNavBar />}
+
+    <div className="mt-5 pt-3">
+      <Container className="" >
+        <Row>
+          <Col xs={12} md={9} lg={9}>
+            <Routes>
+              <Route element={<ProtectedRoutes />}>
+                  <Route path="/" element={<Profile />} />
+                  {/* <Route path="/profile" element={<Profile />} /> */}
+                  <Route path="/profile/:userId" element={<Profile />} />
+                  <Route path="/edit-experience/:userId" element={<EditExperiencePage />} />
+                </Route>
+              
+              <Route element={<GuestRoutes />}>
+                <Route path="/login" element={<Login/>} />
+              </Route>
+              
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Col>
+          {token && <Col xs={12} md={3} lg={3} className="mt-4" >
+            <Sidebar currentUserId={currentUserId} />
+          </Col>}
+        </Row>
+      </Container>
     </div>
-  );
+    <MyFooter />
+  </Router>
+);
 }
 
+  
+  
 export default App;
