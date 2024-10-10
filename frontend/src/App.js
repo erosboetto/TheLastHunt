8c46c1b3129dd34354fdaf85e52275639b119aa6
erosboetto <erosboetto23@gmail.com>
2024-10-10 20:53:24 +0200

    change frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavbarComponent from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import AppRoutes from "./AppRoutes";
+// import AppRoutes from "./AppRoutes";
 import ModalContext from './context/ModalContext';
 import { AuthProvider } from './context/AuthContext';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/home/Home.jsx';
+import Character from './components/character/Character.jsx';
+import Weapon from './components/weapon/Weapon.jsx';
+import Planet from './components/planet/Planet.jsx';
+import SupportForm from './components/help-page/SupportForm.jsx';
+import Requirements from './components/help-page/Requirements.jsx';
+import Info from './components/info/Info.jsx';
 
 const App = () => {
   const [isAuthModalOpen, setAuthModalOpen] = useState(false);
@@ -27,7 +34,15 @@ const App = () => {
           <div className="app-container">
             <NavbarComponent />
             <main className="app-content">
-              <AppRoutes />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/info' element={<Info />} />
+                <Route path='/characters' element={<Character />} />
+                <Route path="/planets" element={<Planet />} />
+                <Route path='/weapons' element={<Weapon />} />
+                <Route path='/support-form' element={<SupportForm />} />
+                <Route path='/requirements' element={<Requirements />} />
+              </Routes>
             </main>
             <Footer />
           </div>
