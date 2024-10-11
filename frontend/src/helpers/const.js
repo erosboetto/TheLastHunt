1d3e42f79a325f70ebd8e769f31cffb79ba6886c
erosboetto <erosboetto23@gmail.com>
2024-10-11 16:44:30 +0200

    deploy

--- frontend/src/helpers/const.js
+++ frontend/src/helpers/const.js
@@ -1,3 +1,3 @@
 export const baseUrl = process.env.NODE_ENV === 'production'
-? 'https://the-last-hunt-backend.vercel.app'
+? 'https://the-last-hunt-backend.netlify.app/'
 : 'http://localhost:4000';
